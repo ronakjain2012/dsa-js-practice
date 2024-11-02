cafce146f4f33e254ad1d552f27ec201e1b1cef7
Ronak Bokaria <Ronakjain2012@gmail.com>
2024-11-02 15:02:03 +0530

    isPalindrome.js

---
+++ input.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+rl.on('close', () => process.exit(0));
+exports.prompt = prompt;
+
+module.exports = exports;
+
+   /**
+    *
+    *
+    **/
---
+++ isPalindrome.js
@@ -0,0 +1,41 @@
+const { prompt } = require('./input');
+const util = require('util');
+const hrtime = process.hrtime;
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  let isPalindrome = true;
+  let Xn = String(x);
+  let j = Xn.length - 1;
+  for (let i = 0; i < Xn.length; i++) {
+    if (Xn[i] != Xn[j]) {
+      isPalindrome = false;
+      break;
+    }
+    if (i === j) {
+      break;
+    }
+    j--;
+  }
+  return isPalindrome;
+
+  /* 
+    7ms Beats 72.80%
+    59.84MB Beats 6.38%
+  */
+};
+
+(async () => {
+  let data = await prompt('Enter a number: ');
+  const start = hrtime();
+  console.log(isPalindrome(data));
+  const end = hrtime(start);
+  const elapsedTime = end[0] * 1000 + end[1] / 1000000; // milliseconds
+  const usedMemory = process.memoryUsage().heapUsed / 1024 / 1024; // MB
+  console.log(`\n\nExecution time: ${elapsedTime.toFixed(2)} ms`);
+  console.log(`Memory usage: ${usedMemory.toFixed(2)} MB`);
+  process.exit(0);
+})();
